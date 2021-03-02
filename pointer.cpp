81e64231f6b2b27928b3cb41c5aa68cbe5bb5f83
Levi McClenny <levimcclenny@tamu.edu>
2021-03-01 20:25:23 -0600

    add pointer

---
+++ pointer.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a = 5;
+
+    int *b;
+    b = &a;
+
+    cout << "a: value: " << a << ", address " << &a << endl;
+    cout << "b: value: " << b << ", address " << &b << endl;
+    cout << "*b = a = " <<*b << endl;
+    cout << endl;
+
+    a = 10;
+
+    cout << "a: value " << a << ", address " << &a << endl;
+    cout << "b: value " << b << ", address " << &b << endl;
+    cout << "*b = a = " <<*b << endl; // dereferencing the pointer
+    cout << endl;
+
+    int c = 3;
+    int &d = c;
+
+    cout << "c: value: " << c << ", address" << &c << endl;
+    cout << "d: value: " << d << ", address" << &d << endl;
+
+    return 0;
+}
